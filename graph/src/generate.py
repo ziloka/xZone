2420b66744c40c50b49adfd5b551426d4e113cbf
Ziloka <50429450+ziloka@users.noreply.github.com>
2023-07-27 09:43:14 -0400

    fix corrupt images #2

--- graph/src/generate.py
+++ graph/src/generate.py
@@ -3,7 +3,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv("../build-vs2019-x64/bin/Debug/image_pubsub_data.csv")
+data = pd.read_csv("../build-vs2019-x64/bin/Debug/image_pubsub_data_176x144.csv")
 
 # Plot points (aka markers in mpl) on a coordinate plane and determine best fit
 
@@ -11,7 +11,7 @@ data = pd.read_csv("../build-vs2019-x64/bin/Debug/image_pubsub_data.csv")
 # ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
 fig, ax = plt.subplots()
 
-ax.set_title('Publisher / Subscriber benchmark image (640 x 480)')
+ax.set_title('Publisher / Subscriber benchmark image (176 x 144)')
 ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel('Latency (μs)') # microseconds, a millionth of a second
 # set axis maybe
@@ -22,6 +22,6 @@ ax.scatter(data["frequency"], data["latency"], s=50, facecolor='C0', edgecolor='
 plt.show()
 
 #                         height width
-fig.savefig('pubsub_image_640_480.png')
+fig.savefig('pubsub_image_176_144.png')
 
 print("generated the graph")
