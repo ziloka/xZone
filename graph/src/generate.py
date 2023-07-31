f0135f54fae7585c9eea34ef41609af526695526
Ziloka <50429450+ziloka@users.noreply.github.com>
2023-07-31 11:15:49 -0400

    generate graph using mean latency values

--- graph/src/generate.py
+++ graph/src/generate.py
@@ -3,7 +3,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv("../build-vs2019-x64/bin/Debug/image_pubsub_data_176x144.csv")
+df = pd.read_csv("../build-vs2019-x64/bin/Debug/image_pubsub_data_176x144.csv")
 
 # Plot points (aka markers in mpl) on a coordinate plane and determine best fit
 
@@ -11,13 +11,16 @@ data = pd.read_csv("../build-vs2019-x64/bin/Debug/image_pubsub_data_176x144.csv"
 # ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
 fig, ax = plt.subplots()
 
+# https://matplotlib.org/stable/tutorials/introductory/quick_start.html#parts-of-a-figure
+
 ax.set_title('Publisher / Subscriber benchmark image (176 x 144)')
 ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel('Latency (μs)') # microseconds, a millionth of a second
 # set axis maybe
-# ax.axis([])
 
-ax.scatter(data["frequency"], data["latency"], s=50, facecolor='C0', edgecolor='k')
+freqs = pd.unique(df["frequency"])
+latencies = [df.loc[df["frequency"] == freq]["latency"].mean() for freq in freqs]
+ax.plot(freqs, latencies, linewidth=2)
 
 plt.show()
 
