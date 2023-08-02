d770f5406691afe0b99532faf2099f572b1c76c9
Ziloka <50429450+ziloka@users.noreply.github.com>
2023-08-02 10:46:29 -0400

    change image

--- graph/src/generate.py
+++ graph/src/generate.py
@@ -2,8 +2,10 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
+import seaborn as sns
 
-df = pd.read_csv("../build-vs2019-x64/bin/Release/image_pubsub_data.csv")
+df = pd.read_csv("../build-vs2019-x64/bin/Debug/image_pubsub_data.csv", engine="pyarrow")
 
 # Plot points (aka markers in mpl) on a coordinate plane and determine best fit
 
@@ -11,19 +13,32 @@ fig, ax = plt.subplots()
 
 # https://matplotlib.org/stable/tutorials/introductory/quick_start.html#parts-of-a-figure
 
-ax.set_title('Publisher / Subscriber benchmark image (176 x 144)')
+ax.set_title('Publisher / Subscriber benchmark image (1920 x 1080)')
 ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel('Latency (μs)') # microseconds, a millionth of a second
 
-freqs = pd.unique(df["frequency"])
+filtered = df[(np.abs(stats.zscore(df["latency"])) < 3) & (np.abs(stats.zscore(df["latency"])) > -3)]
+
+freqs = pd.unique(filtered["frequency"])
+# https://stackoverflow.com/questions/23199796/detect-and-exclude-outliers-in-a-pandas-dataframe
+
+# latencies = []
+# for freq in freqs:
+#     dataFrame = filtered.loc[filtered["frequency"] == freq]
+#     q_low = dataFrame["latency"].quantile(0.01)
+#     q_hi  = dataFrame["latency"].quantile(0.99)
+#     print(dataFrame[(dataFrame["latency"] < q_hi) & (dataFrame["latency"] > q_low)]["latency"].mean(), end = "\n\n")
+#     latencies.append(dataFrame[(dataFrame["latency"] < q_hi) & (dataFrame["latency"] > q_low)]["latency"].mean())
+
+# latencies = [filtered.loc[filtered["frequency"] == freq]["latency"].mean() for freq in freqs]
+
 latencies = [df.loc[df["frequency"] == freq]["latency"].mean() for freq in freqs]
+
 # plot mean line
 ax.plot(freqs, latencies, linewidth=2)
 
 # plot standard deviation line
-
-
-plt.show()
+# plt.show()
 
 #                         height width
 fig.savefig('pubsub_image.png')
