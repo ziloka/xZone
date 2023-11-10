0ebbbc5276ee5426cc83cfdd0d4891075aa9718c
Ziloka <50429450+ziloka@users.noreply.github.com>
2023-11-09 21:56:05 -0500

    make graph error graph

--- graph/src/generate.py
+++ graph/src/generate.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
+from scipy import stats
+import seaborn as sns
 
 # Plot points (aka markers in mpl) on a coordinate plane and determine best fit
 
@@ -9,21 +13,22 @@ fig, ax = plt.subplots()
 # https://matplotlib.org/stable/tutorials/introductory/quick_start.html#parts-of-a-figure
 
 ax.set_title('Publisher / Subscriber benchmark image (1920 x 1080) mean values')
-# ax.set_xlabel('Frequency (Hz)')
+ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel('Latency (μs)') # microseconds, a millionth of a second
 
-def getLatencies(data_filepath: str):
-    return pd.read_csv(data_filepath, engine="pyarrow")["latency"]
+def addLegend(data_filepath: str, transport: str, color: str):
+    df = pd.read_csv(data_filepath, engine="pyarrow")
+    freqs = pd.unique(df["frequency"])
 
-transports = ["Shared Memory", "TCP", "UDP"]
-latencies = [getLatencies("jetson_agx_xavier_shared-mem_1920_1080_image.csv").mean(), getLatencies("jetson_agx_xavier_tcp_1920_1080_image.csv").mean(), getLatencies("jetson_agx_xavier_udp_1920_1080_image.csv").mean()]
-# addLegend(ax, "jetson_agx_xavier_shared-mem_1920_1080_image.csv", "Shared Memory")
-# addLegend(ax, "jetson_agx_xavier_tcp_1920_1080_image.csv", "TCP")
-# addLegend(ax, "jetson_agx_xavier_udp_1920_1080_image.csv", "UDP")
-bar_labels = ['Shared Memory', 'TCP', 'UDP']
-bar_colors = ['tab:red', 'tab:blue', 'tab:orange']
-ax.bar(transports, latencies, label=bar_labels, color=bar_colors)
+    y = [df.loc[df["frequency"] == freq]["latency"].mean() for freq in freqs]
+    plt.scatter(freqs, y, label = transport)
+    err = [df.loc[df["frequency"] == freq]["latency"].std() for freq in freqs]
+    # fmt='none' to prevent the lines from connecting
+    ax.errorbar(freqs, y, xerr=None, yerr=err, ls='none')
 
+addLegend("pc_tcp_1920_1080_image.csv", "TCP", "tab:purple")
+addLegend("pc_udp_1920_1080_image.csv", "UDP", "tab:green")
+addLegend("pc_shared-mem_1920_1080_image.csv", "Shared Memory", 'tab:blue')
 ax.legend()
 
 #                         height width
